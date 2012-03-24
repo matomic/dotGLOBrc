d4569f590c96242730375c6f2388f5400427c4af
Mathieu 張凌峰 <mlfzhang@Zagreus>
2012-03-24 03:19:15 -0700

    + A simple deploy script 	new file:   deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -i bash/bashrc.user      ${HOME}/.bashrc
+cp -i fonts/fonts.conf.user ${HOME}/.fonts.conf
+cp -i vim/vimrc.user        ${HOME}/.vimrc
+cp -i vim/gvimrc.user       ${HOME}/.gvimrc
