01f89285edd776612e050ca1009d1bb31cc9831c
Mathieu 張凌峰 <mlfzhang@Zagreus>
2012-03-28 02:28:12 -0700

    * fix path issue 	modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-cp -i bash/bashrc.user      ${HOME}/.bashrc
-cp -i fonts/fonts.conf.user ${HOME}/.fonts.conf
-cp -i vim/vimrc.user        ${HOME}/.vimrc
-cp -i vim/gvimrc.user       ${HOME}/.gvimrc
+DIR=$(dirname "$0")
+cp -i ${DIR}/bash/bashrc.user      ${HOME}/.bashrc
+cp -i ${DIR}/fonts/fonts.conf.user ${HOME}/.fonts.conf
+cp -i ${DIR}/vim/vimrc.user        ${HOME}/.vimrc
+cp -i ${DIR}/vim/gvimrc.user       ${HOME}/.gvimrc
