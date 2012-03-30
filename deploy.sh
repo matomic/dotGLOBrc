3415aa17af68c37a4bc1ba8d08b820e818de7c37
Mathieu 張凌峰 <mlfzhang@Proteus>
2012-03-30 09:57:52 -0700

    * deploy only if new version is different 	modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
+deploy_if_diff() {
+	[ -n "`diff -q "${1}" "${2}"`" ] && cp -i "${1}" "${2}"
+}
+
 DIR=$(dirname "$0")
-cp -i ${DIR}/bash/bashrc.user      ${HOME}/.bashrc
-cp -i ${DIR}/fonts/fonts.conf.user ${HOME}/.fonts.conf
-cp -i ${DIR}/vim/vimrc.user        ${HOME}/.vimrc
-cp -i ${DIR}/vim/gvimrc.user       ${HOME}/.gvimrc
+
+deploy_if_diff "${DIR}/bash/bashrc.user"      "${HOME}/.bashrc"
+deploy_if_diff "${DIR}/fonts/fonts.conf.user" "${HOME}/.fonts.conf"
+deploy_if_diff "${DIR}/vim/vimrc.user"        "${HOME}/.vimrc"
+deploy_if_diff "${DIR}/vim/gvimrc.user"       "${HOME}/.gvimrc"
