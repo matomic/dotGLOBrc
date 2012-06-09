a7cced0cf72151d1ab4643c0d81327dd81bba538
Mathieu 張凌峰 <mzhang@Andromeda>
2012-06-09 10:08:11 -0700

    deploy_if_diff

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+function deploy_if_diff {
+if [ x !=  x`diff -q "${1}" "${2}"` ]
+then
+	cp -i "${1}" "${2}"
+else
+	printf "${1} and ${2} is identical.\n"
+fi
+}
+
 DIR=$(dirname "$0")
-cp -i ${DIR}/bash/bashrc.user      ${HOME}/.bashrc
-cp -i ${DIR}/fonts/fonts.conf.user ${HOME}/.fonts.conf
-cp -i ${DIR}/vim/vimrc.user        ${HOME}/.vimrc
-cp -i ${DIR}/vim/gvimrc.user       ${HOME}/.gvimrc
+deploy_if_diff ${DIR}/bash/bashrc.user      ${HOME}/.bashrc
+deploy_if_diff ${DIR}/fonts/fonts.conf.user ${HOME}/.fonts.conf
+deploy_if_diff ${DIR}/vim/vimrc.user        ${HOME}/.vimrc
+deploy_if_diff ${DIR}/vim/gvimrc.user       ${HOME}/.gvimrc
