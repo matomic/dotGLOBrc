aa5a6faad82ff2ff6189750f1b1b0f1b52279068
Mathieu 張凌峰 <mlfzhang@Zagreus>
2013-11-14 19:42:36 -0800

    Enable virtualenv and synastic vim plugins

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
+set -x
 
 deploy_if_diff() {
-	diff -uN "${2}" "${1}" || cp -i "${1}" "${2}"
+  diff -uN "${2}" "${1}"
+  if [ -n "`diff -qN \"${2}\" \"${1}\"`" ];
+  then
+    cp -i -L "${1}" "${2}"
+  fi
 }
 
 DIR=$(dirname "$0")
 
 deploy_if_diff "${DIR}/bash/bashrc.user"      "${HOME}/.bashrc"
 deploy_if_diff "${DIR}/fonts/fonts.conf.user" "${HOME}/.fonts.conf"
-deploy_if_diff "${DIR}/vim/vimrc.user"        "${HOME}/.vimrc"
+deploy_if_diff "${DIR}/vim/vimrc.user"        "${HOME}/.vim/vimrc"
 deploy_if_diff "${DIR}/vim/gvimrc.user"       "${HOME}/.gvimrc"
