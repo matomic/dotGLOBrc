6c08eaaa9f087926f395cca636d23a652e485e6b
Mathieu 張凌峰 <mlfzhang{@}outlook{.}com>
2016-05-07 00:19:11 -0700

    Make bashrc moduler to be deployed in ~/.bashrc.d

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,40 @@
-#!/bin/sh
-set -x
+#!/bin/bash
+set +x
 set -e
 
 DIFF=`which colordiff || which diff`
 deploy_if_diff() {
-  ${DIFF} -uN "${2}" "${1}" || true
-  if [ -n "`diff -qN \"${2}\" \"${1}\"`" ];
+  local fr=$1;
+  local to=$2;
+  if [[ -d "${to}" ]]
   then
-    cp -i -L "${1}" "${2}"
+    local to=${to}/`basename ${fr}`
+  fi
+  ${DIFF} -uN "${to}" "${fr}" || true
+  if [[ -n `diff -qN "${to}" "${fr}"` ]];
+  then
+    cp -i -L "${fr}" "${to}"
   fi
 }
 
 pushd $(dirname "$0")
 
-mkdir -p ${HOME}/.vim
+mkdir -p ${HOME}/.bashrc.d/modules
+rsync -av bash/modules/ ${HOME}/.bashrc.d/modules/
+for f in bash/*
+do
+  [[ -f $f ]] && deploy_if_diff $f ${HOME}/.bashrc.d
+done
+[[ -f ${HOME}/.bashrc ]] || ln -s .bashrc.d/bashrc.user ${HOME}/.bashrc
 
-deploy_if_diff "bash/bashrc.user" "${HOME}/.bashrc"
+mkdir -p ${HOME}/.vim
 deploy_if_diff "vim/vimrc.user"   "${HOME}/.vim/vimrc"
 deploy_if_diff "vim/vimrc.local"  "${HOME}/.vim/vimrc.local"
 rsync -av --delete vim/*.vim ${HOME}/.vim/
 [[ -f ${HOME}/.vimrc ]] || ln -s .vim/vimrc ${HOME}/.vimrc
 
 #deploy_if_diff "vim/gvimrc.user"       "${HOME}/.gvimrc"
-if [ -d "${HOME}/.config/fontconfig/" ]
+if [[ -d "${HOME}/.config/fontconfig" ]]
 then
   deploy_if_diff "fonts/fonts.conf.user" "${HOME}/.config/fontconfig/fonts.conf"
 fi
