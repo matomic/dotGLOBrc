70062908cf4c5de07e263008bbc7385c529929e6
Mathieu 張凌峰 <mlfzhang{@}outlook{.}com>
2017-08-09 23:11:55 -0700

    keychain.sh: reimplement keychain init functions.

--- bash/keychain.sh
+++ bash/keychain.sh
@@ -2,35 +2,32 @@
 ## Enable ssh-agent and gpg-agent {{{
 eval_keychain() {
 	# eval keychain, if failed, stop existing ssh-agent and try again
-	eval `keychain --eval $*` || eval `keychain --stop all --eval $*`
+	eval `keychain --eval $*` || (keychain --stop all --quiet; eval $(keychain --stop all --eval $*))
 }
 
 init_keychain() {
-	keychainbin=$(which keychain 2> /dev/null)
-	gpgagentbin=$(which gpg-agent 2> /dev/null)
-	if [ "x${keychainbin}" != "x" ]; then
-		if [ "x${gpgagentbin}" != "x" ]; then
-			eval `keychain --agents gpg --eval`;
+	local keychainbin=$(which keychain 2> /dev/null)
+	local gpgagentbin=$(which gpg-agent 2> /dev/null)
+	if [ "x${keychainbin}" != "x" ]
+	then
+		if [ "x${gpgagentbin}" != "x" ]
+		then
+			eval `keychain --quiet --agents gpg --eval`;
 		fi
-		identities=''
+		local identities=''
 		for id in id_dsa id_rsa id_ecdsa id_ed25519; do
 			if [ -f "${HOME}/.ssh/${id}" ]; then
 				identities+="${identities} ${id}"
 			fi
 		done
-		if [ "x${identities}" != "x" ]; then
+		if [ "x${identities}" != "x" ]
+		then
 			eval_keychain --quiet --agents ssh ${identities} $*
-			exportstr=`keychain --eval --agents ssh ${identities} $* --quiet`
-			alias ssh="${exportstr} /usr/bin/ssh"
+			local exportstr=`keychain --eval --agents ssh ${identities} $* --quiet`
+			#alias ssh="${exportstr} /usr/bin/ssh"
 		fi
 	fi
 }
-#GPG_TTY=$(tty)
-#export GPG_TTY
-init_keychain 2> /dev/null
-init_keychain
 ## }}}
-
-unset keychainbin
 
 ## vim: foldmethod=marker ft=sh noet sta ts=2 sw=2
