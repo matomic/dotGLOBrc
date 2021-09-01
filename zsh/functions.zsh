5a578e7aeb4e08677ba155703052eeb8757b7b75
Mathieu Zhang <mzhang@zoox.com>
2021-08-31 18:06:24 -0700

    zsh: Check-in more functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,5 +1,5 @@
 fpath=($(dirname $0)/functions $fpath)
-autoload +X find_ssh_auth_sock hgrep seqdo argsep
+autoload +X find_ssh_auth_sock hgrep seqdo argsep extract
 # for _zsh in $(dirname $0)/functions/*
 # do
 # 	source $_zsh
