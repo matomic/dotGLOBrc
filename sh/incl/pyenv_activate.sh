384852f462aa18f83879a4401726883894d3451c
Mathieu Zhang <mzhang@zoox.com>
2021-09-01 16:55:40 -0700

    sh: user search path overrides default search path in pyenv_activate

--- sh/incl/pyenv_activate.sh
+++ sh/incl/pyenv_activate.sh
@@ -6,15 +6,14 @@
 # pyenv_activate env_name [search_paths ...]
 #
 pyenv_activate () {
-	local search_paths=( "${@:2}" ./ ~/.virtualenv{,s} ~/.pyenv{,s} ~/.venv{,s} )
+	local search_paths=( ~/.virtualenv{,s} ~/.pyenv{,s} ~/.venv{,s} . );
+	[[ -n "${@:2}" ]] && search_paths=( "${@:2}" )
 	for root in $search_paths
 	do
-		if [[ -f $root/$1/bin/activate ]];
-		then
-			source $root/$1/bin/activate
-			return
-		fi
+		[[ -f "$root/$1/bin/activate" ]] && source "$root/$1/bin/activate" && return
 	done
 	printf "Fail to find activate script for $1 in $search_paths\n" > /dev/stderr
 	return 1
 }
+
+# vim: ft=zsh
