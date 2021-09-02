45596ccb925a86103b0b67063b6b6d0cf77fd6cb
Mathieu 張凌峰 <lingfeng.m.zhang@gmail.com>
2021-09-02 15:55:31 -0700

    bash: fixes keychain.sh

--- xdg_config/bash/keychain.sh
+++ xdg_config/bash/keychain.sh
@@ -10,18 +10,19 @@ eval_keychain() {
 
 init_keychain() {
 	local gpgagentbin=$(which gpg-agent 2> /dev/null)
+	local keychainopts=
 	if which keychain > /dev/null
 	then
 		if [ -n "${gpgagentbin}" ]
 		then
-			eval `keychain --quiet --agents gpg --eval`;
+			eval `keychain $keychainopts --agents gpg --eval`;
 		fi
 		local identities=`ls ${HOME}/.ssh/{id_dsa,id_rsa,id_ecdsa,id_ed25519} 2> /dev/null`
-		if [ "x${identities}" != "x" ]
+		if [ -n "${identities+1}" ]
 		then
-			eval_keychain --quiet --agents ssh ${identities} $*
-			local exportstr=`keychain --eval --agents ssh ${identities} $* --quiet`
-			#alias ssh="${exportstr} /usr/bin/ssh"
+			eval_keychain $keychainopts --agents ssh ${identities} $*
+			# local exportstr=`keychain --eval --agents ssh ${identities} $* --quiet`
+			# alias ssh="${exportstr} /usr/bin/ssh"
 		fi
 	fi
 }
