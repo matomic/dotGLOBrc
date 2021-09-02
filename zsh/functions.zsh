37662acceb29620fcf7a7de99cbc8ab67c036180
Mathieu Zhang <mzhang@zoox.com>
2021-09-01 23:17:10 -0700

    zsh: Enable pyenv_activate

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,6 +1,14 @@
 fpath=($(dirname $0)/functions $fpath)
-autoload +X find_ssh_auth_sock hgrep seqdo argsep extract
+autoload +X \
+	find_ssh_auth_sock \
+	hgrep \
+	seqdo \
+	argsep \
+	extract \
+	pyenv_activate
 # for _zsh in $(dirname $0)/functions/*
 # do
 # 	source $_zsh
 # done
+
+# vim: sh=zsh
