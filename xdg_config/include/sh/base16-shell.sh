de87ca67c542a4254555ec007a795b9548842bda
Mathieu Zhang <mzhang@zoox.com>
2022-01-19 10:19:03 -0800

    sh: add reset_theme_base16 function

--- xdg_config/include/sh/base16-shell.sh
+++ xdg_config/include/sh/base16-shell.sh
@@ -1,12 +1,23 @@
 
 BASE16_SHELL=${1:-${BASHRC_DIR}}/modules/base16-shell/
 
-colortest_base16 () {
-		${BASE16_SHELL}/colortest $*
-}
-
 init_base16() {
 [ -n "$PS1" ] && \
 	[ -s $BASE16_SHELL/profile_helper.sh ] && \
 	eval "` $BASE16_SHELL/profile_helper.sh `"
+}
+
+colortest_base16 () {
+		${BASE16_SHELL}/colortest $*
+}
+
+reset_theme_base16 () {
+	local theme=${1:-${BASE16_THEME}}
+	local script="${BASE16_SHELL}/scripts/base16-${theme}.sh"
+	if declare -f _base16  > /dev/null && [ -f "${script}" ]
+	then
+		set -x
+		_base16 "${script}" "${theme}"
+		set -x
+	fi
 }
--- zsh/base16-shell.zsh
+++
@@ -1,1 +0,0 @@
-../sh/base16-shell.sh
