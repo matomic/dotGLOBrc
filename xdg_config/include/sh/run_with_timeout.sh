34d6955c8a4dc81b5479210e79a0bb235ab9f20e
Mathieu Zhang <mzhang@zoox.com>
2022-01-19 11:01:44 -0800

    sh: add run_with_timeout function

---
+++ xdg_config/include/sh/run_with_timeout.sh
@@ -0,0 +1,20 @@
+# run_with_timeout
+# ====
+# Run a command with timeout
+#
+# run_with_timeout [10] cmd args...
+#
+function run_with_timeout(){
+	local time=10
+	if [[ $1 =~ ^[0-9]+$ ]]; then time=$1; shift; fi
+
+	# Run in a subshell to avoid job control messages
+	( "$@" &
+		local child=$!
+		# Avoid default notification in non-interactive shell for SIGTERM
+		trap -- "" SIGTERM
+		( sleep $time ; kill $child 2> /dev/null ) &
+		wait $child
+	)
+}
+# vim: ft=zsh
