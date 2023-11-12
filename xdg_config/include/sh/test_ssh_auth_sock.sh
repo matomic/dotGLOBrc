9fafc11f170a7d732608e578033608cec766cbf6
Mathieu 張凌峰 <1909262-mlfz@users.noreply.gitlab.com>
2023-11-12 01:52:10 -0800

    sh: fixes test_ssh_auth_sock

--- xdg_config/include/sh/test_ssh_auth_sock.sh
+++ xdg_config/include/sh/test_ssh_auth_sock.sh
@@ -8,11 +8,12 @@
 #
 # return 0 if success, otherwise the return code of ssh-add
 test_ssh_auth_sock () {
-	if ! SSH_AUTH_SOCK=$1 ssh-add -l > /dev/null
+	SSH_AUTH_SOCK=$1 ssh-add -l > /dev/null
+	local retval=$?
+	if [[ $retval -ne 0 ]]
 	then
-		local retval=$?;
 		[[ $retval -eq 1 ]] && return 0;
-		return $retval;
 	fi
+	return $retval;
 }
 # vim: ft=zsh
